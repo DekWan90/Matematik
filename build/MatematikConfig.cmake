00fc5b3370cf4f97fd640247febbc9eadd372cbe
Siti Sarah Kamarudin <ctsarah153@gmail.com>
2018-07-02 06:50:27 +0800

    Add .gitignore

--- build/MatematikConfig.cmake
+++
@@ -1,7 +0,0 @@
-GET_FILENAME_COMPONENT( Matematik_CMAKE_DIR "${CMAKE_CURRENT_LIST_FILE}" PATH )
-
-SET( Matematik_INCLUDE_DIRS "/home/dekwan/Projek/WRT-Map/src/Matematik/include/;/home/dekwan/Projek/WRT-Map/src/Matematik/dev/" )
-
-FIND_LIBRARY( Matematik_CORE NAMES MatematikCore MatematikCored NO_DEFAULT_PATH HINTS "/home/dekwan/Projek/WRT-Map/src/Matematik/lib" )
-
-SET( Matematik_LIBRARIES ${Matematik_CORE} )
--- include/version.h
+++ include/version.h
@@ -1,11 +1,11 @@
 #ifndef VERSION_H
 #define VERSION_H
 
-#define Matematik_VERSION "0.18.02.23.02"
-#define Matematik_VERSION_SHORT "0.18.02"
+#define Matematik_VERSION "0.18.07.02.06"
+#define Matematik_VERSION_SHORT "0.18.07"
 #define Matematik_VERSION_MAJOR 18
-#define Matematik_VERSION_MINOR 02
-#define Matematik_VERSION_RELEASE 23
-#define Matematik_VERSION_BUILD 02
+#define Matematik_VERSION_MINOR 07
+#define Matematik_VERSION_RELEASE 02
+#define Matematik_VERSION_BUILD 06
 
 #endif /* VERSION_H */
